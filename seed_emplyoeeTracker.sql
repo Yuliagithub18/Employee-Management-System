e8bb1618fe8ff23fc82f99d87de77f7331ba9174
Yulia Che <yuliromero87@gmail.com>
2019-12-19 20:42:40 -0800

    changing

--- seed_emplyoeeTracker.sql
+++ seed_emplyoeeTracker.sql
@@ -1,11 +1,33 @@
 DROP DATABASE IF EXISTS employee_tracker_db;
-
 CREATE DATABASE employee_tracker_db;
-
 USE employee_tracker_db;
 
-CREATE TABLE employee (
+CREATE TABLE department (
+id INT NOT NULL AUTO_INCREMENT,
+PRIMARY KEY(id),
+department VARCHAR(30)
+);
+
+CREATE TABLE role (
+id INT NOT NULL AUTO_INCREMENT,
+PRIMARY KEY (id),
+title VARCHAR(30),
+salary DECIMAL,
+department_id INTEGER NOT NULL,
+CONSTRAINT fk_department_id
+FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE manager (
 id INTEGER NOT NULL AUTO_INCREMENT,
+PRIMARY KEY(id),
+manager VARCHAR(30)
+);
+
+CREATE TABLE employee (
+id INT NOT NULL AUTO_INCREMENT,
 PRIMARY KEY (id),
 first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30) NOT NULL,
@@ -21,29 +43,33 @@ FOREIGN KEY (manager_id)
     ON UPDATE CASCADE
 );
 
-CREATE TABLE role (
-id INTEGER NOT NULL AUTO_INCREMENT,
-PRIMARY KEY (id),
-title VARCHAR(30),
-salary DECIMAL,
-department_id INTEGER NOT NULL,
-CONSTRAINT fk_department_id
-FOREIGN KEY (department_id)
-    REFERENCES department(id)
-    ON UPDATE CASCADE
-);
+INSERT into department (department)
+VALUES
+('Sales'),
+('HR');
 
-CREATE TABLE department (
-id INTEGER NOT NULL AUTO_INCREMENT,
-PRIMARY KEY(id),
-department VARCHAR(30)
-);
+INSERT INTO role (title, salary, department_id)
+VALUES
+('Sales lead', 70,000, 1),
+('HR lead', 40,000, 2),
+('Sales assistant', 35,000, 1),
+('HR assistant', 45,000, 2);
 
-CREATE TABLE manager (
-id INTEGER NOT NULL AUTO_INCREMENT,
-PRIMARY KEY(id),
-manager VARCHAR(30)
-);
+INSERT INTO manager (manager)
+VALUES
+('Yulia'),
+('Jill'),
+('Jennifer');
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+("Yulia", "Che", 1, 1),
+("Jill","Black", 2, 1),
+("Jennifer","Hooper", 4, 3),
+("Emily","Ross", 4, 3),
+("Tanya","Who", 4, 3),
+("Lisa","Croft", 3, 2),
+("Kat","Smith", 3, 2);
 
 SELECT * FROM employee_tracker_db.employee;
 
@@ -52,35 +78,6 @@ SELECT * FROM employee_tracker_db.role;
 SELECT * FROM employee_tracker_db.department;
 
 SELECT * FROM employee_tracker_db.manager;
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES
-("Sary", "Jamie", 1, 1),
-("John","Smith", 2, 1),
-("William","Apolo", 4, 3),
-("Monica","Guiller", 4, 3),
-("Aya","Martin", 4, 3),
-("Helen","Gwenn", 3, 2)
-("Arturo","Pandito", 3, 2);
-
-INSERT INTO role (title, salary, department_id)
-VALUES
-('Sales lead', 60,000, 1),
-('HR lead', 60,000, 2),
-('Sales assistant', 45,000, 1),
-('HR assistant', 45,000, 2);
-
-INSERT into department (department)
-VALUES
-('Sales'),
-('HR');
-
-INSERT INTO manager (id, manager)
-VALUES
-('Bob'),
-('Helen'),
-('John');
-
 SELECT
 id, first_name, last_name, title, department, salary, manager
 FROM
@@ -94,10 +91,4 @@ role using (id)
 ORDER BY
 id;
 
-
-
-
-
-
-
-
+SELECT * FROM employee
--- server_employeeTracker.js
+++ server_employeeTracker.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "mysqlmysql",
+    password: "yulia",
     database: "employee_tracker_db"
 });
 
@@ -87,7 +87,7 @@ function allEmployee() {
         console.log(output);
         runSearch();
     });
-    
+
 }
 
 function viewByDepartment() {
